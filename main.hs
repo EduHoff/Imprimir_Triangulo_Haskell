451e82c40d2295c4e4eb69ca7638eb76d3591555
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-09-09 11:50:08 -0300

    criando algumas funções para auxiliar o código principal depois

--- main.hs
+++ main.hs
@@ -1,8 +1,30 @@
 import System.Process (system)
-
-triangulo :: Int -> IO
+{-
+Estrutura base do programa
 triangulo _ = do
-    print"teste" 
+    putStr"_"
+    putStr"*"
+    putStr"_"
+    putStrLn""
+-}
+
+base_triangulo :: Int -> Int
+base_triangulo n
+    | n <= 0 = 0
+    | otherwise = 2 * n - 1
+
+total_triangulo :: Int -> Int
+total_triangulo n
+    | n <= 0 = 0
+    | otherwise = base_triangulo n + total_triangulo (n-1)
+
+triangulo :: Int -> IO ()
+triangulo _ = do
+    putStr"_"
+    putStr"*"
+    putStr"_"
+    putStrLn""
+    
 
 main :: IO()
 main = do
@@ -11,8 +33,11 @@ main = do
     putStr "||"
     altura  <- readLn
     
-    putStr "Distância: "
-    print(triangulo altura)
+    putStrLn "Distância: "
+    triangulo altura
+
+    print(base_triangulo altura)
+    print(total_triangulo altura)
 
 clear :: IO ()
 clear = system "cls || clear" >> return ()
